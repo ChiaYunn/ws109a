df6558f0f14f887ac52ee51ca37a249deb4146ff
janice <janice881188@gmail.com>
2021-01-17 23:52:29 +0800

    oo

---
+++ final/Readme.md
@@ -0,0 +1,39 @@
+# 期末完整網站專案(Chatroom)
+
+## 登入：
+
+![image](./pic/1.jpg)
+
+## 註冊：
+
+### 資料會存入資料庫中
+
+![image](./pic/2.jpg)
+
+## 聊天室：
+
+[Code](https://github.com/ChiaYunn/ws109a/blob/master/final/Server.py)
+
+### 訊息發送後會依時間推送至下方
+
+![image](./pic/3.jpg)
+
+## 參考資料：
+
+1.https://codertw.com/程式語言/3741
+
+2.https://www.youtube.com/watch?v=ZvU57lTnNgo
+
+3.https://ithelp.ithome.com.tw/articles/10213468
+
+4.https://ithelp.ithome.com.tw/articles/10222132
+
+5.https://www.w3schools.com/howto/howto_css_social_login.asp
+
+6.https://pythonprogramming.net/server-chatroom-sockets-tutorial-python-3/
+
+7.https://clay-atlas.com/blog/2019/10/15/python-chinese-tutorial-socket-tcp-ip/
+
+8.https://blog.techbridge.cc/2017/06/03/python-web-flask101-tutorial-introduction-and-environment-setup/
+
+9.https://pythonprogramming.net/client-chatroom-sockets-tutorial-python-3/?completed=/server-chatroom-sockets-tutorial-python-3/
--- final/Server.py
+++ final/Server.py
@@ -39,7 +39,6 @@ class user(db.Model):
 def getTime():
     t = time.localtime()
     timee = time.strftime("%m/%d/%Y, %H:%M:%S", t)
-    print(timee)
     return timee
 
 
@@ -56,12 +55,15 @@ def homeoo():
 @app.route('/room', methods=['GET', 'POST'])
 def homeroo():
     global msg
+    wsl = request.environ.get("wsgi.websocket")
+    if wsl:
+        print(wsl)
     if request.method == 'POST':
         i = request.form['inputm']
         ctime = str(getTime())
         row = {'msg': i, 'time': ctime}
         msg.append(row)
-        _msg = msg
+        _msg = msg.copy()
         _msg.reverse()
         return render_template('chatroom.html', msg=_msg)
     else:
@@ -72,7 +74,8 @@ def homeroo():
 def homeottto():
     u = request.form['Username']
     p = request.form['Password']
-    return redirect('/room')
+
+    return render_template('chatroom.html', userName=u)
 
 
 @app.route('/sign', methods=['POST'])
@@ -93,75 +96,3 @@ def demo_url_for():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-HEADER_LENGTH = 10
-Host = "127.0.0.1"
-Port = 8080
-
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((Host, Port))
-server_socket.listen()
-sockets_list = [server_socket]
-clients = {}
-
-
-def receive_message(client_socket):
-
-    try:
-
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        if not len(message_header):
-            return False
-
-        message_length = int(message_header.decode('utf-8').strip())
-
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-
-    except:
-
-        return False
-
-
-while True:
-
-    read_sockets, _, exception_sockets = select.select(
-        sockets_list, [], sockets_list)
-
-    for notified_socket in read_sockets:
-
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_message(client_socket)
-
-            if user is False:
-                continue
-
-            sockets_list.append(client_socket)
-            clients[client_socket] = user
-
-        else:
-
-            message = receive_message(notified_socket)
-
-            if message is False:
-                print('Closed connection from: {}'.format(
-                    clients[notified_socket]['data'].decode('utf-8')))
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-
-            user = clients[notified_socket]
-
-            for client_socket in clients:
-
-                if client_socket != notified_socket:
-                    client_socket.send(
-                        user['header'] + user['data'] + message['header'] + message['data'])
-
-    for notified_socket in exception_sockets:
-
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
